8ea50d6ebbf9283fc9d0864e42d854726d854615
JesusPinedaC <jesus.pineda@physics.gu.se>
2021-02-18 13:29:00 +0100

    update metrics.py

--- apido/metrics.py
+++ apido/metrics.py
@@ -7,62 +7,22 @@ def mae(P, T):
     return K.mean(K.abs(P - T))
 
 
-norm_factor = {
-    "20x": [1 / 265, 1 / 594, 1 / 641],
-    "40x": [1 / 232, 1 / 615, 1 / 369],
-    "60x": [1 / 201, 1 / 1201, 1 / 312],
-}
-
-
-def w_nuclei(weight, index):
+def w_nuclei(index, weight=1):
     def nuclei(P, T):
         return mae(P[..., index], T[..., index]) * weight
 
     return nuclei
 
 
-def w_lipids(weight, index):
+def w_lipids(index, weight=1):
     def lipids(P, T):
         return mae(P[..., index], T[..., index]) * weight
 
     return lipids
 
 
-def w_cyto(weight, index):
-    def cyto(P, T):
-        return mae(P[..., index], T[..., index]) * weight
-
-    return cyto
+_metrics = [w_nuclei, w_lipids]
 
 
-_metrics = [w_nuclei, w_lipids, w_cyto]
-
-
-def metrics(weights="20x", actions=[1, 2, 3]):
-    weights = [norm_factor[weights][action - 1] for action in actions]
-    return [
-        _metrics[action - 1](weight, index)
-        for index, weight, action in zip(range(10), weights, actions)
-    ]
-
-
-def combined_metric(weights="20x", actions=[1, 2, 3]):
-    """Weighted sum of metrics
-
-    Parameters
-    ----------
-    weights : list, optional
-        A list of weights for the individual metrics. Should be the same
-        length as returned by `metrics`.
-
-    Returns
-    -------
-    Callable[Tensor, Tensor] -> Tensor
-        A tensorflow/keras metrics function.
-    """
-
-    def inner(P, T):
-
-        return sum(metric(P, T) for metric in metrics(weights, actions))
-
-    return inner
+def metrics():
+    return [_metrics[index](index) for index in range(2)]
