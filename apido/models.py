25696caa6eeca5a34ca46bcc59e80b3dafc6cb02
JesusPinedaC <jesus.pineda@physics.gu.se>
2021-02-24 12:54:50 +0100

    update apido/models.py

--- apido/models.py
+++ apido/models.py
@@ -7,7 +7,7 @@ from tensorflow.keras.initializers import RandomNormal
 import numpy as np
 
 
-def generator(breadth, depth):
+def generator(breadth, depth, config):
     """Creates a u-net generator according to the specifications in the paper.
 
     * Uses concatenation skip steps in the encoder
@@ -27,11 +27,15 @@ def generator(breadth, depth):
 
     """
 
-    normalization_layer = layers.Lambda(lambda x: Normalization(x))
+    sub = np.array(config["input_subtract"]).reshape((1, 1, 1, -1))
+    div = np.array(config["input_divide"]).reshape((1, 1, 1, -1))
+    normalization_layer = layers.Lambda(
+        lambda x: K.tanh(2 * (x - sub) / (div - sub) - 1)
+    )
 
     kernel_initializer = RandomNormal(mean=0.0, stddev=0.02)
 
-    activation = layers.LeakyReLU(0.1)
+    activation = layers.LeakyReLU(0.2)
 
     encoder_convolution_block = dt.layers.ResidualBlock(
         activation=activation,
@@ -91,7 +95,7 @@ def generator(breadth, depth):
     return generator
 
 
-def discriminator(depth):
+def discriminator(depth, config):
     """Creates a patch discriminator according to the specifications in the paper.
 
     Parameters
@@ -103,9 +107,18 @@ def discriminator(depth):
 
     """
 
-    normalization_layer = layers.Lambda(lambda x: Normalization(x))
+    sub = np.concatenate(
+        [config["target_subtract"], config["input_subtract"]], axis=-1
+    ).reshape((1, 1, 1, -1))
+    div = np.concatenate(
+        [config["target_divide"], config["input_divide"]], axis=-1
+    ).reshape((1, 1, 1, -1))
 
-    activation = layers.LeakyReLU(0.1)
+    normalization_layer = layers.Lambda(
+        lambda x: K.tanh(2 * (x - sub) / (div - sub) - 1)
+    )
+
+    activation = layers.LeakyReLU(0.2)
 
     discriminator_convolution_block = dt.layers.ConvolutionalBlock(
         kernel_size=(4, 4),
